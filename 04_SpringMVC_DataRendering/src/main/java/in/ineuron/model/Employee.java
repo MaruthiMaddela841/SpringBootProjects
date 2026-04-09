88c6846a56a61b053e1de16f6d55ff35a66e43a9
Maruthi Maddela <maruthimaddela841@gmail.com>
2026-04-10 03:43:57 +0530

    changed project structure

--- 04_SpringMVC_DataRendering/src/main/java/in/ineuron/model/Employee.java
+++ 04_SpringMVC_DataRendering/src/main/java/in/ineuron/model/Employee.java
@@ -13,5 +13,40 @@ public class Employee {
 	private String ename;
 	private String edesg;
 	private Double salary;
+	public Employee(Integer eno, String ename, String edesg, Double salary) {
+		super();
+		this.eno = eno;
+		this.ename = ename;
+		this.edesg = edesg;
+		this.salary = salary;
+	}
+	public Integer getEno() {
+		return eno;
+	}
+	public void setEno(Integer eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getEdesg() {
+		return edesg;
+	}
+	public void setEdesg(String edesg) {
+		this.edesg = edesg;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eno=" + eno + ", ename=" + ename + ", edesg=" + edesg + ", salary=" + salary + "]";
+	}
 
 }
